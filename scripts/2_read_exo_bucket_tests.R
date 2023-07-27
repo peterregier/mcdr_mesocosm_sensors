082142ddb5cfed454a33ea29cff675053665ee99
peterregier <peter.regier@pnnl.gov>
2023-07-26 18:40:11 -0600

    update naming of exo datasets

--- scripts/2_read_exo_bucket_tests.R
+++ scripts/2_read_exo_bucket_tests.R
@@ -1,20 +1,8 @@
-## This script reads in bucket tests conducted with the EXOs, formats them,
-## cleans them, then constructs comparison plots to compare sensor performance
-## and ID potential issues
-## 
-## 2023-06-20
-## Peter Regier
+## This is the start of a script to look at bucket-tests. It's getting far enough
+## to look at the post-cal bucket tests and compare them, but won't go farther today
 ##
-# ########### #
-# ########### #
 
-## Load packages
-require(pacman)
-p_load(tidyverse, # keep things tidy
-       parsedate, # parse_date()
-       janitor, # clean_names()
-       tictoc, # how long will this take...
-       lubridate) # tidy datetime handling
+# 1. Setup ---------------------------------------------------------------------
 
 ## Location of raw EXO datasets
 bucket_directory = "data/exo/bucket_tests"
@@ -25,27 +13,44 @@ source("scripts/0_constants.R")
 
 # 2. Check encoding is UTF-8 for all files prior to importing data -------------
 
-## This function checks that all files are in UTF-8
-check_encoding <- function(directory){
-  ## Create a tibble of all the files
-  files <- tibble(files = paste0(directory, "/", list.files(directory)))
-  
-  encoding_vector <- list()
-  for(i in 1:nrow(files)){
-    encoding_vector[[i]] <- guess_encoding(read_file_raw(files$files[i]))$encoding[1]
-  }
-  
-  wrong_encoding <- tibble(files = files, 
-                           encoding = unlist(encoding_vector)) %>% 
-    filter(encoding != "UTF-8")
-  
-  ## If it's not, throw a warning!
-  if(nrow(wrong_encoding) > 0) warning(paste0("YOU HAVE ENCODING ISSUES BROOOO: "), print(wrong_encoding$files))
-}
-
 ## Check file formats (script needs UTF-8) - should be empty!
 check_encoding(bucket_directory) 
 
 
+# 3. Read in EXO time-series ---------------------------------------------------
+
+## The function read_exo() is now defined in the constants script because it will
+## be used for both exo time-series and the bucket tests
+
+bucket_files <- tibble(file = list.files(path = bucket_directory, full.names = T)) 
+
+df_bare <- bucket_files %>% 
+  filter(grepl("bare", file)) %>% 
+  pull() %>% 
+  map(read_exo) %>% 
+  bind_rows() %>% 
+  rename("p_h1" = p_h_22, "p_h2" = p_h_23)
+
+df_eelgrass <- bucket_files %>% 
+  filter(grepl("eelgrass", file)) %>% 
+  pull() %>% 
+  map(read_exo) %>% 
+  bind_rows() %>% 
+  rename("p_h1" = p_h_21, "p_h2" = p_h_22)
+
+df_bucket <- bind_rows(df_bare %>% mutate(tank = "Bare"), 
+                df_eelgrass %>% mutate(tank = "Eelgrass")) 
+
+comparison_plot <- function(var){
+  df_bucket %>% 
+    filter(datetime > "2023-07-25" & sal_psu > 10) %>% 
+    ggplot(aes(datetime, {{var}}, color = tank)) + 
+    geom_line()
+} 
+
+plot_grid(comparison_plot(sal_psu), 
+          comparison_plot(p_h1), 
+          comparison_plot(p_h2), 
+          comparison_plot(do_perc))
 
 
