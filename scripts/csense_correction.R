901fa5612895375744346945e79a48803c6ef4d0
roachfd <finnegan.roach@pnnl.gov>
2023-08-14 13:54:38 -0700

    Update csense correction script

--- scripts/csense_correction.R
+++ scripts/csense_correction.R
@@ -1,3 +1,7 @@
+#Finn Roach 
+#Code to correct csense data based on a single calibration point (at the moment)
+#Uses the zero intercept provided on the original turner calibration graphs 
+
 require(pacman)
 p_load(tidyverse,
        janitor,
@@ -8,17 +12,13 @@ p_load(tidyverse,
 
 #code below adapted from peter to load in csense data from datalogger files 
 
-## Location of raw EXO datasets
+## Location of raw csense datasets
 raw_filepath = "data/csense/timeseries"
 
 ## Load constants
 source("scripts/0_constants.R")
 
-
-# 2. Read in data --------------------------------------------------------------
-
-## Currently, there should only be one datalogger file. If things get unwieldy,
-## that might change, and we may need a list.files %>% map() path like exo
+#read in the data
 
 ## Set threshold for flagging CO2 based on standard deviation
 co2_sd_threshold = 50 #if the sd > threshold, cull
@@ -27,12 +27,13 @@ co2_sd_threshold = 50 #if the sd > threshold, cull
 #this is just to allow us to get as many data points as possible for our calibration 
 time_interval_cal = "1 min"
 
-## Helper function
+## Helper functions
 mean_ <- function(var){mean({{var}}, na.rm = T)}
 sd_ <- function(var){sd({{var}}, na.rm = T)}
 
 files <- list.files(raw_filepath, pattern = "CR6Series_Table1", full.names = T)
 
+#load in the raw voltages to compute new calibration point 
 read_cr6 <- function(file){
   df_raw <- read_delim(file, skip = 1) %>% 
     slice(3:n()) %>% 
@@ -47,9 +48,10 @@ df_raw <- files %>%
   map(read_cr6) %>% 
   bind_rows()
 
+#make sure the data is in pacific time
 df_raw$datetime <- force_tz(df_raw$datetime, tz = "America/Los_Angeles")
 
-#calibration times
+#calibration times, 410 ppm cal gas 
 bare_start <- "2023-08-07 15:25"
 bare_end <- "2023-08-07 15:51"
 eel_start <- "2023-08-07 15:40"
@@ -60,16 +62,17 @@ eel_end <- "2023-08-07 16:05"
 #eelgrass = SEVoltage_2, serial number: 24100180
 #intercept from turner calibration: (co2 cal gas ppm, sensor output V) = (0, 0.015)
 #so our equation should look like y = m(x-a) + b, in this case b = 0 for both sensors 
-#multiply by 1000 to convert into volts 
+#multiply the turner values by 1000 to convert into volts 
 a_bare = 0.018*1000
 a_eel = 0.015*1000
 
+#select the calibration times 
 cal_bare <- df_raw[df_raw$datetime>bare_start&df_raw$datetime<bare_end,] %>% select(datetime, SEVoltage_1)
 cal_eel <- df_raw[df_raw$datetime>eel_start&df_raw$datetime<eel_end,] %>% select(datetime, SEVoltage_2)
-view(cal_bare)
-view(cal_eel)
+#visualize the calibrations
 ggplot(cal_bare, aes(datetime, SEVoltage_1)) + geom_line()
 ggplot(cal_eel, aes(datetime, SEVoltage_2)) + geom_line()
+#select the most stable part of these curves
 cal_bare <- cal_bare %>% slice(14:18)
 cal_eel <- cal_eel %>% slice(15:20)
 
@@ -81,12 +84,12 @@ x_eel <- mean(cal_eel$SEVoltage_2)
 m_bare = (410-0)/(x_bare-a_bare)
 m_eel = (410-0)/(x_eel-a_eel)
 
-#now we can construct our datasets 
+#now we can construct our datasets using the y = m(x-a) equation 
 df_raw <- df_raw %>% mutate(co2_ppm_bare = m_bare*(SEVoltage_1-a_bare),
                             co2_ppm_eelgrass = m_eel*(SEVoltage_2-a_eel)) %>% 
   select(datetime, co2_ppm_bare, co2_ppm_eelgrass)
 
-view(df_raw)
+
 #reformat the data-set so it uses the 5 min time interval from the constants script 
 df_raw <- df_raw %>%
   mutate(datetime_pdt = round_date(datetime, unit = time_interval)) %>% 
@@ -97,9 +100,10 @@ df_raw <- df_raw %>%
             co2_ppm_bare = mean_(co2_ppm_bare), 
             co2_ppm_eelgrass = mean_(co2_ppm_eelgrass))
 
-view(df_raw)
+#save this dataframe as a new corrected csv file 
+write_csv(df_raw, "data/csense_timeseries_corrected.csv")
 
-#now let's plot some data 
+#now let's plot some data to see how it looks 
 start1 <- "2023-07-13 00:00"
 end1 <- "2023-07-20 00:00"
 
@@ -112,4 +116,3 @@ ggplot() +
   theme_set(theme_bw()) + theme(plot.title = element_text(hjust = 0.5, size = 12))+
   scale_color_manual(values=c("royalblue","forestgreen"))
 
-write_csv(df_raw, "data/csense_timeseries_corrected.csv")
