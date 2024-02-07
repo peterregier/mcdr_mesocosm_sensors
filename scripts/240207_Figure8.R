a327d413d1526a8a76990c987e902a269ffc84e7
Peter Regier <peter.regier@pnnl.gov>
2024-02-07 13:23:26 -0700

    update data and scripts for paper v1

---
+++ scripts/240207_Figure8.R
@@ -0,0 +1,99 @@
+## This script creates Figure 8, which is a series of time-series plots
+## Source data created by 231210_prep_timeseries.Rmd and then CO2 corrected by
+## 
+## Variables to include: Temp, pH, DO, salinity, CO2
+##
+## 2024-02-07
+## Peter Regier
+##
+# ########### #
+
+source("scripts/0_constants.R")
+
+plot_start <- "2023-08-04"
+plot_end <- "2023-08-21"
+
+df <- read_csv("data/240207_timeseries_corrected.csv") %>% 
+  filter(datetime > plot_start & 
+           datetime < plot_end) %>% 
+  drop_na()
+
+seacarb_output <- carb(flag = 21, 
+                       var1 = df$co2_ppm_calc, 
+                       var2 = df$p_h2, 
+                       T = df$temp_c, 
+                       S = df$sal_psu) %>% 
+  mutate(datetime = df$datetime, 
+         dic_mol_L = DIC * 1.025) %>% # convert kg to L (1.025 kg/L for seawater)
+  as_tibble() %>% 
+  clean_names()
+
+## This might be right? Not sure. But: 
+# (mol CaCO3/kg seawater) * (~100g CaCO3 / 1 mol CaCO3) * 
+# (1000 mg / g) * (1.03 kg / L seawater)
+## 
+# https://www.ultimatereef.net/pages/alkalinity_converter/: 50 ppm = 1 meq/L
+# 1 meq/L = 1000 meq/m3
+df_combined <- df %>% 
+  bind_cols(seacarb_output %>% select(alk, omega_aragonite)) %>% 
+  mutate(alk_mol_kg = alk) %>% 
+  mutate(alk_umol_kg = alk * 1000000) %>% #we're assuming this is mol of charge, per pg 7 of https://www.soest.hawaii.edu/oceanography/courses/OCN623/Spring2012/CO2pH.pdf
+  mutate(alk_meq_kg = alk_mol_kg) %>% #unnecessary, keep for clarity
+  mutate(alk_meq_m3 = alk_meq_kg * (1.03*1000)) #(meq/kg)*(1.03kg/L seawater)*(1000L/m3)
+# mutate(alk_ppm_caco3 = alk * 100 * 1000 * 1.03) %>% 
+# mutate(alk_meqm3 = alk_ppm_caco3 * (1000/50))
+
+make_plot <- function(var, y_label){
+  
+  ## Set scaling for geom_rect and matching axes
+  rect_start <- as_datetime("2023-08-07 10:00:00")
+  y_min = min(df_combined %>% drop_na() %>% pull({{var}}))
+  y_max = max(df_combined %>% drop_na() %>% pull({{var}}))
+  
+  p1 <- ggplot(df_combined, aes(datetime, {{var}}, color = tank)) + 
+    geom_line(show.legend = F) + 
+    annotate(geom = "rect", 
+             xmin = rect_start,
+             xmax = as_datetime("2023-08-07 20:00:00"), 
+             ymin = y_min, 
+             ymax = y_max, 
+             alpha = 0.2) + 
+    annotate(geom = "text", 
+             x = rect_start + hours(36), 
+             y = y_max, 
+             label = "Maintenance", 
+             size = 2) + 
+    labs(x = "", y = y_label) + 
+    scale_color_manual(values=c("royalblue","forestgreen"))
+  
+  p2 <- df_combined %>%
+    mutate(tod = hour(datetime)) %>%
+    group_by(tank, tod) %>%
+    summarize(min = min({{var}}, na.rm = T),
+              max = max({{var}}, na.rm = T),
+              mean = mean({{var}}, na.rm = T)) %>%
+    ggplot(aes(tod, mean, color = tank)) +
+    geom_errorbar(aes(ymin = min, 
+                      ymax = max), 
+                  alpha = 0.5) + 
+    geom_point() + 
+    labs(x = "Time of day", 
+         y = y_label, 
+         color = "") + 
+    scale_color_manual(values=c("royalblue","forestgreen"))
+  
+  plot_grid(p1, p2, nrow = 1, rel_widths = c(1, 0.7))
+}
+
+plot_grid(make_plot(temp_c, "Temp. (C)"), 
+          make_plot(p_h2, "pH"), 
+          make_plot(do_mgl, "DO (mg/L)"), 
+          make_plot(co2_ppm_calc, "CO2 (ppm)"), 
+          make_plot(alk_mol_kg, "Alk (mol/kg)"), 
+          ncol = 1)
+ggsave("figures/240207_Fig8_v1.png", width = 8, height = 12)
+
+
+
+
+
---
+++ scripts/240207_co2_corrections.R
@@ -0,0 +1,145 @@
+## This script is based on Finn's csense_correction_timeseries.R script. Notes from Finn:
+# I tried doing a calibration by forcing air through the pumped head, but I didn't
+# have enough time to work on that method before I left so I didn't get a good
+# calibration down. So instead of using any of this calibration check data, I used
+# the grab sample timeseries data (I think from one full day timeseries), and I
+# paired each CO2 ppm measurement given by picaro with the csense voltage data point
+# that was taken nearest to when the grab sample was collected. This is assuming
+# that picaro was accurate enough that we could treat those points as calibration
+# points (which is a flawed assumption for sure, but I think it was our best option).
+# I then had a script that constructs a calibration line (voltage vs ppm essentially)
+# with this data, and also the zero data point from the factory calibration
+# (I kept this because we didn't have anything close to a zero from the picaro data).
+# It then applies this calibration line to the voltage data from csense.  The script
+# that does this is called csense_correction_timeseries.R and is under scripts in the github.
+## tl;dr - we're correcting C-Sense mv to grab samples by matching a time-series
+## 1. Read in CO2 time-series
+## 2. Read in grab samples
+## 3. Match and plot (that's basically Figure 4)
+## 4. Export for Fig 4
+## 5. Export corrected CO2 for Fig 8
+
+# 1. Setup ---------------------------------------------------------------------
+source("scripts/0_constants.R")
+
+csense <- read_csv("data/240207_timeseries_raw.csv")
+
+## Set threshold for flagging CO2 based on standard deviation
+co2_sd_threshold = 50 #if the sd > threshold, cull
+
+#set new time interval in this script, different than in the constants script 
+#this is just to allow us to get as many data points as possible for our calibration 
+time_interval_cal = "1 min"
+
+## Helper functions
+mean_ <- function(var){mean({{var}}, na.rm = T)}
+sd_ <- function(var){sd({{var}}, na.rm = T)}
+
+## Read in grab samples
+## multiply the CO2 means by 1.052 to account for the volume of gas used to create the headspace
+grab_samples <- read_excel("data/Bi-weekly_Tank_Sampling_2023.xlsx",4) %>% 
+  select(contains("Sample ID"), contains("CO2 mean"), contains("CO2 stdev"), contains("Date"), contains("Time Collected (PSD)")) %>%
+  clean_names() %>% 
+  mutate(date = as_date(date), 
+         time_collected_psd = as_hms(time_collected_psd), 
+         datetime = round_date(as_datetime(paste(date, time_collected_psd)), "5 min")) %>% 
+  mutate(co2_mean = 1.052*co2_mean, 
+         tank = case_when(sample_id == 3 ~ "Bare", 
+                          sample_id == 5 ~ "Eelgrass", 
+                          TRUE ~ NA))
+
+#grab_sample_start <- "2023-07-13 09:00"
+#grab_sample_end <- "2023-07-13 16:10"
+
+
+df1 <- inner_join(csense, grab_samples, 
+                 by = c("datetime", "tank")) %>% 
+  #filter(datetime > "2023-08-15") %>% # only keep 8/16 samples
+  select(datetime, contains("co2_m"), tank)
+
+ggplot(df1, aes(co2_mean, co2_mv, color = as.factor(as_date(datetime)))) + 
+  geom_point() + 
+  geom_smooth(method = "lm") + 
+  #stat_poly_eq() +
+  facet_wrap(~tank) + 
+  labs(x = "Grab-sample CO2 (ppm)", 
+       y = "Sensor CO2 (mV)")
+ggsave("figures/240207_grab_v_sensor_co2_all.png", width = 7, height = 3)
+
+df1_07_08 <- df1 %>% 
+  filter(datetime > "2023-07-10") 
+write_csv(df1_07_08, "data/240207_co2_713_816_for_fig4.csv")
+
+ggplot(df1_07_08, aes(co2_mean, co2_mv)) + 
+  geom_point() + 
+  geom_smooth(method = "lm") + 
+  #stat_poly_eq() +
+  facet_wrap(tank~as_date(datetime)) + 
+  labs(x = "Grab-sample CO2 (ppm)", 
+       y = "Sensor CO2 (mV)")
+ggsave("figures/240207_grab_v_sensor_co2_713_816.png", width = 7, height = 6)
+
+ggplot(df1_07_08 %>% filter(datetime > "2023-08-01") %>% 
+       mutate(tod = as.factor(hour(datetime))), aes(co2_mean, co2_mv)) + 
+  geom_point(aes(size = tod), alpha = 0.8) + 
+  geom_smooth(method = "lm", aes(color = tank), show.legend = F) + 
+  facet_wrap(~tank, nrow = 1, scales = "free") + 
+  labs(x = "Grab-sample CO2 (ppm)", 
+       y = "Sensor CO2 (mV)", 
+       size = "Hour \n of day") + 
+  scale_color_manual(values=c("royalblue","forestgreen"))
+ggsave("figures/240207_Fig4_v1.png", width = 7, height = 3)
+
+
+
+#bare = SEVoltage_1, serial number: 24100179
+#intercept from turner calibration: (co2 cal gas ppm, sensor output V) = (0, 0.018)
+#eelgrass = SEVoltage_2, serial number: 24100180
+#intercept from turner calibration: (co2 cal gas ppm, sensor output V) = (0, 0.015)
+#multiply the turner values by 1000 to convert into volts 
+a_bare = 0.018*1000
+a_eel = 0.015*1000
+
+## This is the start for the "good" data we'll use
+fig8_start <- "2023-07-28"
+
+df2 <- df1_07_08 %>% 
+  filter(datetime > fig8_start) #%>% 
+  #add_row(datetime = as_datetime("2023-08-16"), co2_mv = a_bare, co2_mean = 0, tank = "Bare") %>% 
+  #add_row(datetime = as_datetime("2023-08-16"), co2_mv = a_eel, co2_mean = 0, tank = "Eelgrass")
+
+## Adding in rows above to force y-intercepts doesn't do a good job, if the intent
+## is simply to ground-truth sensors to Picarro, let's 
+ggplot(df2, aes(co2_mean, co2_mv, color = tank)) + 
+  geom_point() + 
+  geom_smooth(method = "lm", se = F)
+
+## Let's pivot and just find m and b, then correct CO2. For y=mx+b, we want to
+## predict ppm (Picarro), so that's y (contrary to relationships above)
+bare_lm <- summary(lm(co2_mean ~ co2_mv, data = df2 %>% filter(tank == "Bare")))
+eelgrass_lm <- summary(lm(co2_mean ~ co2_mv, data = df2 %>% filter(tank == "Eelgrass")))
+
+## Cheatsheet to remember which is which
+bare_lm[[4]][2,1] #slope
+bare_lm[[4]][1,1] #int
+round(bare_lm[[9]], 2) #r2
+
+## Double-check that corrections make sense prior to applying to dataset
+df2 %>% 
+  mutate(slope = case_when(tank == "Bare" ~ bare_lm[[4]][2,1], 
+                           tank == "Eelgrass" ~ eelgrass_lm[[4]][2,1]), 
+         int = case_when(tank == "Bare" ~ bare_lm[[4]][1,1], 
+                         tank == "Eelgrass" ~ eelgrass_lm[[4]][1,1])) %>% 
+  mutate(co2_ppm_calc = (co2_mv * slope) + int)
+
+csense_cor <- csense %>% 
+  mutate(slope = case_when(tank == "Bare" ~ bare_lm[[4]][2,1], 
+                           tank == "Eelgrass" ~ eelgrass_lm[[4]][2,1]), 
+         int = case_when(tank == "Bare" ~ bare_lm[[4]][1,1], 
+                         tank == "Eelgrass" ~ eelgrass_lm[[4]][1,1])) %>% 
+  mutate(co2_ppm_calc = (co2_mv * slope) + int)
+write_csv(csense_cor, "data/240207_timeseries_corrected.csv")
+
+
+
+
