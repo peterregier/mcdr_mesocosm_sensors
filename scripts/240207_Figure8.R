c8a913db662e161388d79cce7a32e342a00baf4f
Peter Regier <peter.regier@pnnl.gov>
2024-02-12 16:37:28 -0700

    update figure 8

--- scripts/240207_Figure8.R
+++ scripts/240207_Figure8.R
@@ -43,14 +43,17 @@ df_combined <- df %>%
 # mutate(alk_ppm_caco3 = alk * 100 * 1000 * 1.03) %>% 
 # mutate(alk_meqm3 = alk_ppm_caco3 * (1000/50))
 
-make_plot <- function(var, y_label){
+make_plot <- function(var, resolution, y_label){
   
   ## Set scaling for geom_rect and matching axes
   rect_start <- as_datetime("2023-08-07 10:00:00")
   y_min = min(df_combined %>% drop_na() %>% pull({{var}}))
   y_max = max(df_combined %>% drop_na() %>% pull({{var}}))
+  alpha_level = 0.4
   
   p1 <- ggplot(df_combined, aes(datetime, {{var}}, color = tank)) + 
+    geom_ribbon(aes(ymin = {{var}} - resolution, ymax = {{var}} + resolution, fill = tank), 
+                color = NA, alpha = alpha_level, show.legend = F) + 
     geom_line(show.legend = F) + 
     annotate(geom = "rect", 
              xmin = rect_start,
@@ -64,36 +67,38 @@ make_plot <- function(var, y_label){
              label = "Maintenance", 
              size = 2) + 
     labs(x = "", y = y_label) + 
-    scale_color_manual(values=c("royalblue","forestgreen"))
+    scale_color_manual(values=c("royalblue","forestgreen")) + 
+    scale_fill_manual(values=c("royalblue","forestgreen"))
   
   p2 <- df_combined %>%
     mutate(tod = hour(datetime)) %>%
     group_by(tank, tod) %>%
-    summarize(min = min({{var}}, na.rm = T),
-              max = max({{var}}, na.rm = T),
+    summarize(sd = sd({{var}}, na.rm = T),
+              #max = max({{var}}, na.rm = T),
               mean = mean({{var}}, na.rm = T)) %>%
     ggplot(aes(tod, mean, color = tank)) +
-    geom_errorbar(aes(ymin = min, 
-                      ymax = max), 
+    geom_ribbon(aes(ymin = mean - resolution, ymax = mean + resolution, fill = tank), 
+                color = NA, alpha = alpha_level, show.legend = F) + 
+    geom_errorbar(aes(ymin = mean - sd, 
+                      ymax = mean + sd), 
                   alpha = 0.5) + 
     geom_point() + 
     labs(x = "Time of day", 
          y = y_label, 
          color = "") + 
-    scale_color_manual(values=c("royalblue","forestgreen"))
+    scale_color_manual(values=c("royalblue","forestgreen")) + 
+    scale_fill_manual(values=c("royalblue","forestgreen"))
   
   plot_grid(p1, p2, nrow = 1, rel_widths = c(1, 0.7))
 }
 
-plot_grid(make_plot(temp_c, "Temp. (C)"), 
-          make_plot(p_h2, "pH (NBS)"), 
-          make_plot(do_mgl, "DO (mg/L)"), 
-          make_plot(co2_ppm_calc, "CO2 (ppm)"), 
-          make_plot(alk_umol_kg, "Alk (umol/kg)"), 
+plot_grid(make_plot(temp_c, 0.2, "Temp. (C)"), #Accuracy is 0.2 C per EXO manual
+          make_plot(p_h2, 0.2, "pH (NBS)"), #Accuracy is 0.2 units per EXO manual
+          make_plot(do_mgl, 0.1, "DO (mg/L)"), #Accuracy is 0.1 mg/L per EXO manual
+          make_plot(co2_ppm_calc, 0.03*2000, "CO2 (ppm)"), #Based on accuracy of 3% of full scale
+          make_plot(alk_umol_kg, 0.1, "Alk (umol/kg)"), 
           ncol = 1)
-ggsave("figures/240207_Fig8_v1.png", width = 8, height = 12)
-
-
+ggsave("figures/240207_Fig8_v1.png", width = 10, height = 12)
 
 
 
