62371d14bfb4ca6a4a0a7fdd9c9912c4534c09e6
Peter Regier <peter.regier@pnnl.gov>
2024-04-04 10:25:30 -0600

    adding alternate ways to propagate uncertainty to alkalinity

--- scripts/240402_Figure8.R
+++ scripts/240402_Figure8.R
@@ -114,3 +114,144 @@ ggsave("figures/240207_Fig8_v1.png", width = 8, height = 12)
 
 
 
+## I don't think that this way of estimating errors is incorporating the actual 
+## sensor resolution, but rather the estimated uncertainty of each parameter, potentially
+## estimated from the datasets. Let's do some exps to see how each parameter fed
+## to seacarb::carb impacts alkalinity
+
+alter_alk <- function(var, transformation, label){
+  
+  x <- df %>% 
+    mutate({{var}} := {{var}} + transformation)
+  
+  carb(flag = 21, 
+       var1 = x$co2_ppm_calc, 
+       var2 = x$p_h2, 
+       T = x$temp_c, 
+       S = x$sal_psu) %>% 
+    mutate(datetime = x$datetime, 
+           dic_mol_L = DIC * 1.025) %>% # convert kg to L (1.025 kg/L for seawater)
+    as_tibble() %>% 
+    clean_names() %>% 
+    mutate(alk_umol_kg = alk * 1000000, 
+           label = label) %>% 
+    dplyr::select(datetime, label, alk_umol_kg)
+}
+
+mean_base_alk = alter_alk(co2_ppm_calc, 1, "base") %>% summarize(median(alk_umol_kg)) %>% pull()
+
+## Based on our plot, everything either relates positively or doesn't relate, so 
+## we can run two simple scenarios, one where everything increases and one where
+## everything decreases
+
+bind_rows(alter_alk(co2_ppm_calc, 1, "base"), 
+          alter_alk(co2_ppm_calc, 60, "co2+60"), 
+          alter_alk(co2_ppm_calc, -60, "co2-60"), 
+          alter_alk(p_h2, 0.2, "ph+0.2"), 
+          alter_alk(p_h2, -0.2, "ph-0.2"), 
+          alter_alk(temp_c, 0.2, "temp+0.2"), 
+          alter_alk(temp_c, -0.2, "temp-0.2"), 
+          alter_alk(sal_psu, 0.2, "sal+0.2"), 
+          alter_alk(sal_psu, -0.2, "sal-0.2")) %>% 
+  ggplot(aes(label, alk_umol_kg)) + 
+  geom_boxplot() + 
+  geom_hline(yintercept = mean_base_alk, linetype = "dashed")
+  
+
+## Now let's clunkily make three time-series and calcluate mean offsets: 
+
+alk_comparison <- bind_rows(carb(flag = 21, 
+               var1 = df$co2_ppm_calc, 
+               var2 = df$p_h2, 
+               T = df$temp_c, 
+               S = df$sal_psu) %>% 
+            mutate(datetime = df$datetime, 
+                   tank = df$tank) %>% 
+            as_tibble() %>% 
+            clean_names() %>% 
+            mutate(alk_umol_kg = alk * 1000000, 
+                   label = "alk_umol_kg") %>% 
+            dplyr::select(datetime, label, alk_umol_kg, tank), 
+          carb(flag = 21, 
+               var1 = df$co2_ppm_calc - 60, 
+               var2 = df$p_h2 - 0.2, 
+               T = df$temp_c - 0.2, 
+               S = df$sal_psu - 0.2) %>% 
+            mutate(datetime = df$datetime, 
+                   tank = df$tank) %>% 
+            as_tibble() %>% 
+            clean_names() %>% 
+            mutate(alk_umol_kg = alk * 1000000, 
+                   label = "alk_min") %>% 
+            dplyr::select(datetime, label, alk_umol_kg, tank), 
+          carb(flag = 21, 
+               var1 = df$co2_ppm_calc + 60, 
+               var2 = df$p_h2 + 0.2, 
+               T = df$temp_c + 0.2, 
+               S = df$sal_psu + 0.2) %>% 
+            mutate(datetime = df$datetime, 
+                   tank = df$tank) %>% 
+            as_tibble() %>% 
+            clean_names() %>% 
+            mutate(alk_umol_kg = alk * 1000000, 
+                   label = "alk_max") %>% 
+            dplyr::select(datetime, label, alk_umol_kg, tank))
+          
+
+ggplot(alk_comparison, aes(datetime, alk_umol_kg, color = label)) + 
+  geom_line() + 
+  facet_wrap(~tank)
+
+
+
+alk_wide <- alk_comparison %>% 
+  pivot_wider(names_from = "label", values_from = "alk_umol_kg") %>% 
+  unnest()
+
+rect_start <- as_datetime("2023-08-07 10:00:00")
+y_min = min(alk_wide %>% drop_na() %>% pull(alk_min))
+y_max = max(alk_wide %>% drop_na() %>% pull(alk_max))
+alpha_level = 0.4
+
+p1 <- ggplot(alk_wide, aes(datetime, alk_umol_kg, color = tank)) + 
+  geom_ribbon(aes(ymin = alk_min, ymax = alk_max, fill = tank), 
+              color = NA, alpha = alpha_level, show.legend = F) + 
+  geom_line(show.legend = F) + 
+  annotate(geom = "rect", 
+           xmin = rect_start,
+           xmax = as_datetime("2023-08-07 20:00:00"), 
+           ymin = y_min, 
+           ymax = y_max, 
+           alpha = 0.2) + 
+  annotate(geom = "text", 
+           x = rect_start + hours(36), 
+           y = y_max, 
+           label = "Maintenance", 
+           size = 2) + 
+  labs(x = "", y = "Alkalinity (umol/kg)") + 
+  scale_color_manual(values=c("royalblue","forestgreen")) + 
+  scale_fill_manual(values=c("royalblue","forestgreen"))
+
+p2 <- alk_wide %>%
+  mutate(tod = hour(datetime)) %>%
+  group_by(tank, tod) %>%
+  summarize(min = mean(alk_min, na.rm = T),
+            max = mean(alk_max, na.rm = T),
+            mean = mean(alk_umol_kg, na.rm = T)) %>%
+  ggplot(aes(tod, mean, color = tank)) +
+  geom_ribbon(aes(ymin = min, ymax = max, fill = tank), 
+              color = NA, alpha = alpha_level, show.legend = F) + 
+  geom_errorbar(aes(ymin = min, 
+                    ymax = max), 
+                alpha = 0.5) + 
+  geom_point() + 
+  labs(x = "Time of day", 
+       y = "Alkalinity (umol/kg)", 
+       color = "") + 
+  scale_color_manual(values=c("royalblue","forestgreen")) + 
+  scale_fill_manual(values=c("royalblue","forestgreen"))
+
+plot_grid(p1, p2, nrow = 1, rel_widths = c(1, 0.7))
+ggsave("figures/240404_alkalinity_w_calculated_resolution.png", width = 8, height = 3)
+
+
